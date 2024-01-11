0fbd70219fe9910f33481891f38a0d734f4f3cb2
rust-enjoyer <rust-enjoyer@gmail.com>
2024-01-11 18:47:30 +0000

    added parsing to repl

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,17 +1,40 @@
-use monke_lang_interpreter::lexer::lexer::Lexer;
+use monke_lang_interpreter::{
+    lexer::lexer::Lexer,
+    parser::{ast::Node, parser::Parser},
+};
 use std::io::{self, Result, Write};
 
 fn main() -> Result<()> {
+    const MONKEY_FACE: &str = r#"
+             __,__
+     .--.  .-"    "-. .--.
+    / .. \/ .-. .-. \/ .. \
+    | | '|  /  Y  \  |' | |
+    | \  \ \ 0 | 0 / /  / |
+     \ '- ,\.-""""-./, -'/
+      ''-' /_ ^ ^ _\ '-''
+          | \._ _./ |
+          \  \'~'/  /
+           '._'-'_.'
+             '---'
+"#;
     let mut buffer = String::new();
-    io::stdout().write_all(b"Enter your monke code >> ")?;
+    io::stdout().write_all(b"Enter your monke code\n>>")?;
     io::stdout().flush()?;
 
     while let Ok(_) = io::stdin().read_line(&mut buffer) {
-        let mut lexer = Lexer::new(buffer.clone());
+        let lexer = Lexer::new(buffer.clone());
+        let mut parser = Parser::new(lexer);
 
-        while let Some(token) = lexer.next_token() {
-            println!("{token:?}");
-        }
+        let program = parser.parse_program();
+
+        match program {
+            Ok(p) => println!("{}", p.pretty_print()),
+            Err(err) => {
+                println!("{MONKEY_FACE}");
+                println!("{err}");
+            }
+        };
 
         buffer.clear();
         io::stdout().write_all(b">> ")?;
