f03603272f0eed3934b2bfd84a0a13b9aede53eb
rust-enjoyer <pakhv420@gmail.com>
2024-03-19 19:35:28 +0000

    feat: starting traversing ast... recursively Aware... will change later to a loop

--- monke-lang-interpreter/src/compiler/compiler.rs
+++ monke-lang-interpreter/src/compiler/compiler.rs
@@ -1,4 +1,11 @@
-use crate::{code::code::Instructions, evaluator::types::Object, parser::ast::Program};
+use std::{rc::Rc, usize};
+
+use crate::{
+    code::code::{make, Instructions, OpCodeType},
+    evaluator::types::{Integer, Object},
+    parser::ast::{Expression, Program, Statement},
+    result::InterpreterResult,
+};
 
 #[derive(Debug)]
 pub struct Compiler {
@@ -20,7 +27,51 @@ impl Compiler {
         }
     }
 
-    pub fn compile(&self, node: Program) {}
+    pub fn compile(&mut self, program: Program) -> InterpreterResult<()> {
+        match program {
+            Program::Statements(statements) => {
+                for statement in statements {
+                    self.compile(statement.into())?;
+                }
+
+                Ok(())
+            }
+            Program::Statement(statement) => match statement.as_ref() {
+                Statement::Let(_) => todo!(),
+                Statement::Return(_) => todo!(),
+                Statement::Expression(expression_statement) => {
+                    self.compile(Rc::clone(&expression_statement.expression).into())
+                }
+                Statement::Block(_) => todo!(),
+            },
+            Program::Expression(expression) => match expression.as_ref() {
+                Expression::Identifier(_) => todo!(),
+                Expression::IntegerLiteral(int_expression) => {
+                    let int = Object::Integer(Integer {
+                        value: int_expression.value,
+                    });
+                    let operand = self.add_constant(int);
+                    self.emit(OpCodeType::Constant, vec![operand as i32]);
+
+                    Ok(())
+                    //todo!()
+                }
+                Expression::StringLiteral(_) => todo!(),
+                Expression::Prefix(_) => todo!(),
+                Expression::Infix(infix_expression) => {
+                    self.compile(Rc::clone(&infix_expression.left).into())?;
+                    self.compile(Rc::clone(&infix_expression.right).into())
+                }
+                Expression::Boolean(_) => todo!(),
+                Expression::If(_) => todo!(),
+                Expression::FunctionLiteral(_) => todo!(),
+                Expression::Call(_) => todo!(),
+                Expression::ArrayLiteral(_) => todo!(),
+                Expression::IndexExpression(_) => todo!(),
+                Expression::HashLiteral(_) => todo!(),
+            },
+        }
+    }
 
     pub fn byte_code(&self) -> ByteCode {
         ByteCode {
@@ -28,12 +79,31 @@ impl Compiler {
             instructions: self.instructions.clone(),
         }
     }
+
+    fn add_constant(&mut self, obj: Object) -> usize {
+        self.constants.push(obj);
+        self.constants.len() - 1
+    }
+
+    fn emit(&mut self, op: OpCodeType, operands: Vec<i32>) -> usize {
+        let instructions = make(op, operands);
+        self.add_instructions(instructions)
+    }
+
+    fn add_instructions(&mut self, instructions: Instructions) -> usize {
+        let new_instruction_position = self.instructions.len();
+
+        for byte in instructions {
+            self.instructions.0.push(byte);
+        }
+
+        new_instruction_position
+    }
 }
 
 #[cfg(test)]
 mod test {
     use core::panic;
-    use std::usize;
 
     use crate::{
         code::code::{get_definition, make, read_operands, Definition, Instructions, OpCodeType},
@@ -84,8 +154,11 @@ mod test {
             assert!(program.is_ok());
             let program = program.unwrap();
 
-            let compiler = Compiler::new();
-            compiler.compile(program);
+            let mut compiler = Compiler::new();
+
+            if let Err(err) = compiler.compile(program) {
+                panic!("{err}");
+            }
 
             let byte_code = compiler.byte_code();
 
