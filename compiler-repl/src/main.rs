25ef540d57617c02feee1641768cef74a2cbc623
rust-enjoyer <pakhv420@gmail.com>
2024-03-23 13:42:58 +0000

    feat: added compiler-vm repl

--- compiler-repl/src/main.rs
+++ compiler-repl/src/main.rs
@@ -1,7 +1,9 @@
 use monke_lang_interpreter::{
+    compiler::compiler::Compiler,
     evaluator::{environment::Environment, evaluator::eval, types::Object},
     lexer::lexer::Lexer,
     parser::parser::Parser,
+    vm::vm::Vm,
 };
 use std::{
     cell::RefCell,
@@ -9,8 +11,7 @@ use std::{
     rc::Rc,
 };
 
-fn main() -> Result<()> {
-    const MONKEY_FACE: &str = r#"
+const MONKEY_FACE: &str = r#"
              __,__
      .--.  .-"   "-.  .--.
     / .. \/ .-. .-. \/ .. \
@@ -23,6 +24,8 @@ fn main() -> Result<()> {
            '._'-'_.'
              '---'
 "#;
+
+fn main() -> Result<()> {
     let mut buffer = String::new();
     io::stdout().write_all(b"Enter your monke code\n>> ")?;
     io::stdout().flush()?;
@@ -36,22 +39,30 @@ fn main() -> Result<()> {
 
         let program = parser.parse_program();
 
-        match program {
-            Ok(p) => match eval(p, env) {
-                Ok(result) => match result {
-                    Object::Function(_) => (),
-                    _ => println!("{}\n", result),
-                },
-                Err(err) => {
-                    println!("{MONKEY_FACE}");
-                    println!("{err}");
-                }
-            },
-            Err(err) => {
-                println!("{MONKEY_FACE}");
-                println!("{err}");
-            }
-        };
+        if let Err(err) = &program {
+            print_error(err, &mut buffer)?;
+        }
+
+        let program = program.unwrap();
+
+        let mut compiler = Compiler::new();
+
+        if let Err(err) = &compiler.compile(program) {
+            print_error(err, &mut buffer)?;
+        }
+
+        let byte_code = compiler.byte_code();
+        let mut vm = Vm::new(byte_code);
+
+        if let Err(err) = &vm.run() {
+            print_error(err, &mut buffer)?;
+        }
+
+        let stack_elem = vm.stack_top();
+        assert!(stack_elem.is_some());
+        let stack_elem = stack_elem.unwrap();
+
+        println!("{stack_elem}");
 
         buffer.clear();
         io::stdout().write_all(b">> ")?;
@@ -59,4 +70,13 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn print_error(error: &str, buffer: &mut String) -> Result<()> {
+    println!("{MONKEY_FACE}");
+    println!("{error}");
+
+    buffer.clear();
+    io::stdout().write_all(b">> ")?;
+    io::stdout().flush()
 }
